dd2cd6c83745584081a47067e59bb401b7a08a97
mariano <mariotaebox08@gmail.com>
2022-05-25 16:48:55 -0300

    armado de carpetas

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ const path=require("path");
 const app=express();
 
 
+/*Routes*/
+ app.get('/',(req ,res)=>{
+        res.sendFile(path.resolve(__dirname, './views/home.html'));
+ });
+
+
 
 app.listen(3000, ()=> console.log('server corriendo') )
